8c2104f5d0e53f87b6c18d6ef8e7047f04d5e999
Tara Hassani <70331037+Tara78@users.noreply.github.com>
2022-10-26 14:55:06 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { quiz } from 'reducers/quiz';
+import { Navigation } from 'components/Navigation';
+
+const reducer = combineReducers({
+  quiz: quiz.reducer
+});
+
+const store = configureStore({ reducer });
+
+export const App = () => {
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  );
+};
---
+++ src/components/CurrentQuestion.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { quiz } from 'reducers/quiz';
+
+export const CurrentQuestion = () => {
+  const question = useSelector(
+    (store) => store.quiz.questions[store.quiz.currentQuestionIndex]
+  );
+  const questions = useSelector((store) => store.quiz.questions.length);
+
+  const dispatch = useDispatch();
+
+  if (!question) {
+    return <h1>Oh no! I could not find the current question!</h1>;
+  }
+
+  // const onAnswerSubmit = (id, index) => {
+  //   dispatch(quiz.actions.submitAnswer({ questionId: id, answerIndex: index }))
+  // }
+  const onAnswerSubmit = (questionId, answerIndex) => {
+    dispatch(quiz.actions.submitAnswer({ questionId, answerIndex }));
+    if (question.correctAnswerIndex === answerIndex) {
+      dispatch(quiz.actions.goToNextQuestion());
+    } else {
+      window.alert('Sorry, wrong answer!!');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Question: {question.questionText}</h1>
+      {question.options.map((option, index) => {
+        return (
+          <button
+            onClick={() => onAnswerSubmit(question.id, index)}
+            key={option}
+            type="button"
+          >
+            {option}
+          </button>
+        );
+      })}
+
+      <p>
+        Question {question.id}/{questions}
+      </p>
+    </div>
+  );
+};
